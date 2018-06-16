6ebf0e48141e739ecf0fb2635761ac824cbeb237
Maximillian Xavier <maximillianfx@gmail.com>
2018-06-16 16:12:19 -0300

    Inclusao de conversao de todas as classes de pacientes

--- convertNII2png.py
+++ convertNII2png.py
@@ -19,8 +19,10 @@ iterador = 0
 #Arquivos
 #ADNI_subject_MR_description.nii
 
+print('Conversor de arquivos .nii para PNG')
 (listaSubjectsAD,listaDescriptionAD,listaSubjectsMCI,listaDescriptionMCI,listaSubjectsCN,listaDescriptionCN) = obterSubjectsDescription()
 
+print('Convertendo arquivos de pacientes AD...')
 for subject, description in zip(listaSubjectsAD,listaDescriptionAD):
 	folder = '../Images/'+classes[0]+'/'+subject+'/'+formatarDescription(description)+'/'
 	file = base+'_'+subject+'_'+tipo+'_'+formatarDescription(description)+formato
@@ -34,6 +36,37 @@ for subject, description in zip(listaSubjectsAD,listaDescriptionAD):
 		plt.imsave(diretorio+'img'+str(iterador)+'.png',arr=segmentos[:,i,:],cmap='gray')
 		iterador += 1
 	iterador = 0
+print('Conversao OK')
+print('Convertendo arquivos de pacientes CN...')
+for subject, description in zip(listaSubjectsCN,listaDescriptionCN):
+	folder = '../Images/'+classes[1]+'/'+subject+'/'+formatarDescription(description)+'/'
+	file = base+'_'+subject+'_'+tipo+'_'+formatarDescription(description)+formato
+	arquivo = nib.load(folder+file)
+	image = arquivo.get_data()
+	segmentos = image[:,(image.shape[1] // 2):(image.shape[1] // 2)+50,:]
+	diretorio = '../Output/Images/'+classes[1]+'/'+subject+'/'
+	if not os.path.exists(diretorio):
+		os.makedirs(diretorio)
+	for i in range(50):
+		plt.imsave(diretorio+'img'+str(iterador)+'.png',arr=segmentos[:,i,:],cmap='gray')
+		iterador += 1
+	iterador = 0
+print('Conversao OK')
+print('Convertendo arquivos de pacientes MCI...')
+for subject, description in zip(listaSubjectsMCI,listaDescriptionMCI):
+	folder = '../Images/'+classes[2]+'/'+subject+'/'+formatarDescription(description)+'/'
+	file = base+'_'+subject+'_'+tipo+'_'+formatarDescription(description)+formato
+	arquivo = nib.load(folder+file)
+	image = arquivo.get_data()
+	segmentos = image[:,(image.shape[1] // 2):(image.shape[1] // 2)+50,:]
+	diretorio = '../Output/Images/'+classes[2]+'/'+subject+'/'
+	if not os.path.exists(diretorio):
+		os.makedirs(diretorio)
+	for i in range(50):
+		plt.imsave(diretorio+'img'+str(iterador)+'.png',arr=segmentos[:,i,:],cmap='gray')
+		iterador += 1
+	iterador = 0
+print('Conversoes finalizadas!')
 	#plt.imshow(segmento,cmap='gray')
 	#plt.title('Imagem - ' + str(iterador))
 	#iterador += 1
